68ca34c11c9d21e3117c855c2a940bec82b277ce
Todd Krieger <twkriege@mtu.edu>
2021-09-20 22:04:31 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(street) {
+    return Math.abs(42 - street);
+}
+
+function distanceFromHqInFeet(block) {
+    return Math.abs((42 - block) * 264);
+}
+
+function distanceTravelledInFeet(street1, street2) {
+    return Math.abs((street1 - street2) * 264);
+}
+
+function calculatesFarePrice(start, destination) {
+    let distance = Math.abs((start - destination) * 264);
+    let fair = 0;
+    if (distance <= 400) {
+        fair = 0;
+    }
+    else if (distance <= 2000) {
+        fair = ((distance - 400) * 0.02);
+    }
+    else if (distance > 2500) {
+        fair = 'cannot travel that far';
+    }
+    else {
+        fair = 25.00;
+    }
+    return fair;
+}
